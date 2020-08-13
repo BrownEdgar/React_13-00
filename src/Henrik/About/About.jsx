20241f3bb1ccfe958f9b3147d53e9f5db776846f
BrownEdgar <nikogosjanedgar@gmail.com>
2020-08-13 14:55:16 +0400

    work with ReactRouter part2

--- src/Henrik/About/About.jsx
+++ src/Henrik/About/About.jsx
@@ -2,9 +2,10 @@ import React, { Component } from 'react'
 
 export default class About extends Component {
     render() {
+       
         return (
             <>
-                <h1>{this.props.name}</h1>
+            <h1>Karevor ej</h1>
             </>
         )
     }
