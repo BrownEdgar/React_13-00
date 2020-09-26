7a64bf9388f3c92f3241b9224ff126c33b695045
TAYGRAN <bambukyan.tiko@gmail.com>
2020-09-26 17:15:15 +0400

    My first homework

--- src/Tigran/App.js
+++ src/Tigran/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import Das1 from "./Das1"
 
 export default class App extends Component {
 	render() {
 		return (
 			<div>
-				
+				<Das1/>
 			</div>
 		)
 	}
---
+++ src/Tigran/Das1.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+export default class Das1 extends Component {
+  state = {
+    count: 0,
+    titile: 12
+  }
+
+  counterPlus() {
+    this.setState({
+      count: this.state.count + 10
+    })
+  }
+  counterMinus() {
+    this.setState({
+      count: this.state.count - 5
+    })
+  }
+  reset() {
+    this.setState({
+      count: 0
+    })
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.titile * this.state.count}</h1>
+        <button onClick={this.counterPlus}>+</button>
+        <button onClick={this.counterMinus}>-</button>
+        <button onClick={this.reset}>reset</button>
+      </div>
+    )
+  }
+}
