d2d2d53f1b689ef0223a832e374211033e845154
BrownEdgar <nikogosjanedgar@gmail.com>
2020-09-26 17:16:28 +0400

    add Henry homework

--- src/Henry/App.js
+++ src/Henry/App.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react'
-
-export default class App extends Component {
-	render() {
-		return (
-			<div>
-				
-			</div>
-		)
-	}
+export default class App extends Components{
+  state = {
+      title:"Tenas darav te che",
+      count:0
+  }
+  changeTitle = () => {
+      this.setState({
+          title:"vonc vor darav :)"
+      })
+  }
+  render(){
+      return(
+          <div>
+              <h1>{this.state.count}</h1>
+              <button onClick={() => this.setState({count:this.state.count + 10})}>+</button>
+              <button onClick={() => this.setState({count:this.state.count - 5})}>-</button>
+              <button onClick={() => this.setState({count:0})}>0</button>
+          </div>
+      )
+  }
 }
