865b642f163386fc1a06aedf90896393858a7598
BrownEdgar <nikogosjanedgar@gmail.com>
2020-10-17 17:48:29 +0400

    add LifeCycle example

--- src/Edgar/LifeCycle/Test.js
+++ src/Edgar/LifeCycle/Test.js
@@ -1,32 +1,44 @@
 import React, { Component } from 'react'
 import LifeComponent from './LifeComponent';
 
+
 export default class Test extends Component {
 	constructor(props) {
-		super(props);
-		console.log("------------------------");
 		console.log("constructor run");
+		super(props);
 		this.state = {
 			count: 0,
 
 		}
 	}
-	UNSAFE_componentWillMount(){
-		console.log('UNSAFE_componentWillMount'); 
-	}
-	   
-	
-	componentDidMount() {
-		console.log("------------------------");
-		console.log("componentDidMount");
-	}
 
+static getDerivedStateFromProps(props, state) {
+	console.log(state);
+	return null
+}
+componentDidMount() {
+	console.log("sax nkarvac en");
+}
+shouldComponentUpdate(nextProps, nextState) {
+	if (nextState.count >=30){
+		console.log("Stop");
+		return false;
+	}
+	console.log("der jamanaky che!");
+	return true
+}
 
+componentWillReceiveProps(nextProps) {
+	console.log("nextProps", nextProps);
+}
 	render() {
-		console.log("------------------------");
 		console.log("render run");
 		return (
-		<div>{this.state.count}</div>
+		<div>
+			{this.state.count}
+			<button onClick={()=>this.setState({ count:this.state.count + 1  })}>Up count</button>
+			<LifeComponent n={this.state.count}/>
+		</div>
 		)
 	}
 }
