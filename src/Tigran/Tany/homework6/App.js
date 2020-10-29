3ac7cffcd4bc5c089cc7bb06de5d42a6908b7087
BrownEdgar <nikogosjanedgar@gmail.com>
2020-10-29 17:02:35 +0400

    add folders

--- src/Tigran/Tany/homework6/App.js
+++ src/Tigran/Tany/homework6/App.js
@@ -1,23 +1,26 @@
 import React, { useState } from 'react'
 
 export default function App() {
-    const [pizza, setpizza] = useState([
-        { id: 0, name: "Peperoni" },
-        { id: 1, name: "Margarita" },
-        { id: 2, name: "Diablo" },
-        { id: 3, name: "Altono" },
+	const [pizza, setpizza] = useState([
+		{ id: 0, name: "Peperoni" },
+		{ id: 1, name: "Margarita" },
+		{ id: 2, name: "Diablo" },
+		{ id: 3, name: "Altono" },
 
-    ])
-    const [count, setcount] = useState(0)
-    const counter = () => {
-        setcount(count + 1)
-    }
-    return (
-        <div>
-            {pizza.map((elem, index) => {
-                return <div key={index} id={index}>{`Pizza name:${elem.name}`} {`you have ${count} pizzas`}<button key={index} id={index} onClick={counter}>+</button></div>
-            })}
-        </div>
-        //chi stacvum sax counternery miajamanak en avelanum
-    )
+	])
+	const [count, setcount] = useState(0)
+	const counter = () => {
+		setcount(count + 1)
+	}
+	return (
+		<div>
+			{pizza.map((elem, index) => {
+				return <div key={index} id={index} className={`class-${index}`}>
+					{`Pizza name:${elem.name}`} 
+					{`you have ${count} pizzas`}
+					<button key={index} id={index} onClick={counter}>+</button></div>
+			})}
+		</div>
+		//chi stacvum sax counternery miajamanak en avelanum
+	)
 }
