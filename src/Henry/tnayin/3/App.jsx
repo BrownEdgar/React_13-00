9ef65eed05b250626fc9de7102a957cc0869e430
BrownEdgar <nikogosjanedgar@gmail.com>
2020-11-03 16:53:21 +0400

    add some changes

--- src/Henry/tnayin/3/App.jsx
+++ src/Henry/tnayin/3/App.jsx
@@ -8,29 +8,29 @@ export default class App extends Component {
 		return (
 			<div>
 				<>
-				<h1 className='title'>Info</h1>
-                <div className='box'>
-				<MyContext.Provider value={{
-					name: "Name: Henry",
-                    surname:"Surname: Ghambaryan",
-                    age:'Age: 19',
-                    email:"E-mail: ghambaryanh@gmail.com",
-                    phone:"Phone: +374(33)-32-1X-XX)"
-				}}>
-					<Context />
-				</MyContext.Provider>
-                </div>
-				<div className='box'>
-				<MyContext.Provider value={{
-					name: "Name: Tigran",
-                    surname:"Surname: Bambukyan",
-                    age:'Age: 19',
-                    email:"E-mail: bambukyan.tiko@gmail.com",
-                    phone:"Phone: +374(94)-87-7X-XX)"
-				}}>
-					<Context />
-				</MyContext.Provider>
-                </div>
+					<h1 className='title'>Info</h1>
+					<div className='box'>
+						<MyContext.Provider value={{
+							name: "Name: Henry",
+							surname: "Surname: Ghambaryan",
+							age: 'Age: 19',
+							email: "E-mail: ghambaryanh@gmail.com",
+							phone: "Phone: +374(33)-32-1X-XX)"
+						}}>
+							<Context />
+						</MyContext.Provider>
+					</div>
+					<div className='box'>
+						<MyContext.Provider value={{
+							name: "Name: Tigran",
+							surname: "Surname: Bambukyan",
+							age: 'Age: 19',
+							email: "E-mail: bambukyan.tiko@gmail.com",
+							phone: "Phone: +374(94)-87-7X-XX)"
+						}}>
+							<Context />
+						</MyContext.Provider>
+					</div>
 				</>
 			</div>
 		)
