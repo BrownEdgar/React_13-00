8ef6e95ab839e970de836c489d567f5aa0e7d20e
TAYGRAN <bambukyan.tiko@gmail.com>
2020-11-03 05:10:16 -0800

    m

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from "./Tigran/Tany/homework7/App";
-
+import App from "./Tigran/Texum/Img_gallary/App";
 
 
 ReactDOM.render(
-   <React.StrictMode>
-      <App/>
-     </React.StrictMode>,
+   // <React.StrictMode>
+      <App/>,
+   //   </React.StrictMode>,
    document.getElementById('root'));
