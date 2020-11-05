3694c8d86aa4222d31a231610b526b8b2723582a
BrownEdgar <nikogosjanedgar@gmail.com>
2020-11-05 17:04:19 +0400

    dasd tema shopping cart

--- src/Edgar/shopping-cart/Carts.jsx
+++ src/Edgar/shopping-cart/Carts.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-import { Card } from '@material-ui/core';
+
 import Cart from './Cart';
 export default function Carts(props) {
 	return (
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from "./Tigran/Texum/Accordion_web/App";
+import App from "./Edgar/shopping-cart/App";
 
 
 ReactDOM.render(
