0e9dec327ce5bf2ef2b9263e5e276bd880928b8e
Henr55 <ghambaryanh@gmail.com>
2020-11-16 15:47:07 +0400

    demo

--- src/Henry/tnayin/4/App.js
+++ src/Henry/tnayin/4/App.js
@@ -1,27 +1,16 @@
 import React, { Component } from 'react'
 import "./App.css"
-import Helpers from "./Helpers"
+import Skizb from './Skizb'
+import Mejtex from "./Mejtex"
+import Verj from "./Verj"
 
 export default class App extends Component {
     render() {
         return (
-            <div id="box">
-          <div id='first'>
-              <h1>"</h1>
-            <p>Nunc consectetur a nunc vitae pretium. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque eu ullamcorper mauris. Aenean convallis, ligula eu dignissim commodo, sem ligula vulputate dui, eget auctor</p>
-            <h3>Roman Level / AppName</h3>  
-          </div>
-          <div id='second'>
-          <h1>"</h1>
-            <p>Nunc consectetur a nunc vitae pretium. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque eu ullamcorper mauris. Aenean convallis, ligula eu dignissim commodo, sem ligula vulputate dui, eget auctor</p>
-            <h3>Diana Rinzuk / AppName</h3>
-          </div>
-          <div id='third'>
-          <h1>"</h1>
-          <p>Nunc consectetur a nunc vitae pretium. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque eu ullamcorper mauris. Aenean convallis, ligula eu dignissim commodo, sem ligula vulputate dui, eget auctor</p> 
-          <h3>Ben Stafford / AppName</h3>
-          </div>
-          <Helpers/>
+            <div>
+          <Skizb/>
+          <Mejtex/>
+          <Verj/>
                 </div>
         )
     }
---
+++ src/Henry/tnayin/4/Verj.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class Last extends Component {
+    render() {
+        return (
+            <div id="last">
+                <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"></link>
+                <ul>
+                <li><i id="z" class="fab fa-facebook-f"></i></li>
+                <li><i id="b" class="fab fa-twitter"></i></li>
+                <li><i id="c" class="fab fa-instagram"></i></li>
+                </ul>
+                <div id="last1">
+                    <p>Made by <span>Cruip</span>.All right reserved.</p>
+                    <h2>Contact</h2>
+                    <h2>About Us</h2>
+                    <h2>FAQ's</h2>
+                    <h2>Support</h2>
+                </div>
+            </div>
+        )
+    }
+}
